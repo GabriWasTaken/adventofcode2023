4dce11f05c5d0910ff8107318f4d0b2107da4b3c
gabriele <gabrieletorta@gmail.com>
2023-12-07 09:41:07 +0100

    day5

--- src/Day5.js
+++ src/Day5.js
@@ -15,9 +15,9 @@ function Day5() {
           .toString()
           .substring(
             baseText.toString().indexOf(searchString) + searchString.length + 1,
-            baseText.toString().indexOf("\n\n")
+            baseText.toString().indexOf("\r\n\r\n")
           )
-          .split("\n");
+          .split("\r\n");
       }
       map = map.map((row) => row.split(" "));
       return map;
@@ -28,13 +28,8 @@ function Day5() {
       seeds.map((seed) => {
         let founded = false;
         firstMap.map((eachMatch) => {
-          if (
-            Number(seed) >= Number(eachMatch[1]) &&
-            Number(seed) <= Number(eachMatch[1]) + Number(eachMatch[2])
-          ) {
-            soils.push(
-              Number(seed) - Number(eachMatch[1]) + Number(eachMatch[0])
-            );
+          if (Number(seed) >= Number(eachMatch[1]) && Number(seed) <= Number(eachMatch[1]) + Number(eachMatch[2])) {
+            soils.push(Number(seed) - Number(eachMatch[1]) + Number(eachMatch[0]));
             founded = true;
           }
         });
@@ -58,55 +53,36 @@ function Day5() {
       .then((r) => r.text())
       .then((text) => {
         if (text.toString().includes("seeds:")) {
-          seedsArray = text
-            .toString()
-            .substring(7, text.toString().indexOf("\n"))
-            .split(" ");
+          seedsArray = text.toString().substring(7, text.toString().indexOf("\r\n")).split(" ");
         }
-        text = text
-          .toString()
-          .substring(text.toString().indexOf("\n\n") + 1, text.lenght);
+        text = text.toString().substring(text.toString().indexOf("\r\n\r\n") + 1, text.lenght);
 
         firstMap = mapping(text.toString(), "seed-to-soil map:");
-        text = text
-          .toString()
-          .substring(text.toString().indexOf("\n\n") + 1, text.lenght);
+        text = text.toString().substring(text.toString().indexOf("\r\n\r\n") + 1, text.lenght);
 
         secondMap = mapping(text.toString(), "soil-to-fertilizer map:");
-        text = text
-          .toString()
-          .substring(text.toString().indexOf("\n\n") + 1, text.lenght);
+        text = text.toString().substring(text.toString().indexOf("\r\n\r\n") + 1, text.lenght);
 
         thirdMap = mapping(text.toString(), "fertilizer-to-water map:");
-        text = text
-          .toString()
-          .substring(text.toString().indexOf("\n\n") + 1, text.lenght);
+        text = text.toString().substring(text.toString().indexOf("\r\n\r\n") + 1, text.lenght);
 
         fourthMap = mapping(text.toString(), "water-to-light map:");
-        text = text
-          .toString()
-          .substring(text.toString().indexOf("\n\n") + 1, text.lenght);
+        text = text.toString().substring(text.toString().indexOf("\r\n\r\n") + 1, text.lenght);
 
         fifthMap = mapping(text.toString(), "light-to-temperature map:");
-        text = text
-          .toString()
-          .substring(text.toString().indexOf("\n\n") + 1, text.lenght);
+        text = text.toString().substring(text.toString().indexOf("\r\n\r\n") + 1, text.lenght);
 
         sixMap = mapping(text.toString(), "temperature-to-humidity map:");
-        text = text
-          .toString()
-          .substring(text.toString().indexOf("\n\n") + 1, text.lenght);
+        text = text.toString().substring(text.toString().indexOf("\r\n\r\n") + 1, text.lenght);
 
         if (text.toString().includes("humidity-to-location map:")) {
           sevenMap = text
             .toString()
             .substring(
-              text.toString().indexOf("humidity-to-location map:") +
-                "humidity-to-location map:".length +
-                1,
+              text.toString().indexOf("humidity-to-location map:") + "humidity-to-location map:".length + 1,
               text.toString().lenght
             )
-            .split("\n");
+            .split("\r\n");
         }
         sevenMap = sevenMap.map((row) => row.split(" "));
 
@@ -123,7 +99,7 @@ function Day5() {
   };
 
   const calculatePart2 = () => {
-    let min = 999999;
+    let min = 9999999999;
 
     const mapping = (baseText, searchString) => {
       let map = [];
@@ -131,10 +107,10 @@ function Day5() {
         map = baseText
           .toString()
           .substring(
-            baseText.toString().indexOf(searchString) + searchString.length + 1,
-            baseText.toString().indexOf("\n\n")
+            baseText.toString().indexOf(searchString) + searchString.length + 2,
+            baseText.toString().indexOf("\r\n\r\n")
           )
-          .split("\n");
+          .split("\r\n");
       }
       map = map.map((row) => row.split(" "));
       return map;
@@ -143,11 +119,8 @@ function Day5() {
     const seedToSoil = (seed, firstMap) => {
       let soil;
       let founded = false;
-      firstMap.forEach( (eachMatch) => {
-        if (
-          Number(seed) >= Number(eachMatch[1]) &&
-          Number(seed) < Number(eachMatch[1]) + Number(eachMatch[2])
-        ) {
+      firstMap.forEach((eachMatch) => {
+        if (Number(seed) >= Number(eachMatch[1]) && Number(seed) < Number(eachMatch[1]) + Number(eachMatch[2])) {
           soil = Number(seed) - Number(eachMatch[1]) + Number(eachMatch[0]);
           founded = true;
           return null;
@@ -172,60 +145,42 @@ function Day5() {
       .then((r) => r.text())
       .then((text) => {
         if (text.toString().includes("seeds:")) {
-          seedsArray = text
-            .toString()
-            .substring(7, text.toString().indexOf("\n"))
-            .split(" ");
+          seedsArray = text.toString().substring(7, text.toString().indexOf("\r\n")).split(" ");
         }
-        text = text
-          .toString()
-          .substring(text.toString().indexOf("\n\n") + 1, text.lenght);
+        text = text.toString().substring(text.toString().indexOf("\r\n\r\n") + 1, text.lenght);
 
         firstMap = mapping(text.toString(), "seed-to-soil map:");
-        text = text
-          .toString()
-          .substring(text.toString().indexOf("\n\n") + 1, text.lenght);
+        text = text.toString().substring(text.toString().indexOf("\r\n\r\n") + 1, text.lenght);
 
         secondMap = mapping(text.toString(), "soil-to-fertilizer map:");
-        text = text
-          .toString()
-          .substring(text.toString().indexOf("\n\n") + 1, text.lenght);
+        text = text.toString().substring(text.toString().indexOf("\r\n\r\n") + 1, text.lenght);
 
         thirdMap = mapping(text.toString(), "fertilizer-to-water map:");
-        text = text
-          .toString()
-          .substring(text.toString().indexOf("\n\n") + 1, text.lenght);
+        text = text.toString().substring(text.toString().indexOf("\r\n\r\n") + 1, text.lenght);
 
         fourthMap = mapping(text.toString(), "water-to-light map:");
-        text = text
-          .toString()
-          .substring(text.toString().indexOf("\n\n") + 1, text.lenght);
+        text = text.toString().substring(text.toString().indexOf("\r\n\r\n") + 1, text.lenght);
 
         fifthMap = mapping(text.toString(), "light-to-temperature map:");
-        text = text
-          .toString()
-          .substring(text.toString().indexOf("\n\n") + 1, text.lenght);
+        text = text.toString().substring(text.toString().indexOf("\r\n\r\n") + 1, text.lenght);
 
         sixMap = mapping(text.toString(), "temperature-to-humidity map:");
-        text = text
-          .toString()
-          .substring(text.toString().indexOf("\n\n") + 1, text.lenght);
+        text = text.toString().substring(text.toString().indexOf("\r\n\r\n") + 1, text.lenght);
 
         if (text.toString().includes("humidity-to-location map:")) {
           sevenMap = text
             .toString()
             .substring(
-              text.toString().indexOf("humidity-to-location map:") +
-                "humidity-to-location map:".length +
-                1,
+              text.toString().indexOf("humidity-to-location map:") + "humidity-to-location map:".length + 2,
               text.toString().lenght
             )
-            .split("\n");
+            .split("\r\n");
         }
         sevenMap = sevenMap.map((row) => row.split(" "));
 
         seedsArray.forEach((seed, index) => {
           if (index % 2 === 0) {
+            console.log(index);
             for (let i = 0; i < seedsArray[index + 1]; i++) {
               const soil = seedToSoil(Number(seed) + i, firstMap);
               const fertilizer = seedToSoil(soil, secondMap);
@@ -246,57 +201,9 @@ function Day5() {
             }
           }
         });
-
-        console.log(min);
+        console.log("-------------------------");
+        console.log("completed: ", min);
       });
-  };
-
-  const CalculatePart3 = () => {
-    fetch(raw)
-      .then((r) => r.text())
-      .then((input) => {
-        const [seedsLine, ...mappingLines] = input.split("\n\n");
-        const seeds = seedsLine.split(":")[1].trim().split(" ").map(Number);
-        const mapMatrix = mappingLines.map((line) =>
-          line
-            .split("\n")
-            .slice(1)
-            .map((s) => s.split(" ").map(Number))
-            .map(([dStart, sStart, length]) => ({
-              dStart,
-              dEnd: dStart + length - 1,
-              sStart,
-              sEnd: sStart + length - 1,
-            }))
-        );
-
-        const candidateSeeds = mapMatrix
-          .flatMap((mappings, i) =>
-            mappings.map((m) =>
-              mapMatrix.slice(0, i + 1).reduceRight((curr, mm) => {
-                const n = mm.find((n) => curr >= n.dStart && curr <= n.dEnd);
-                return n ? n.sStart + (curr - n.dStart) : curr;
-              }, m.dStart)
-            )
-          )
-          .filter((seed) =>
-            seeds.some(
-              (s, i) => i % 2 === 0 && seed >= s && seed < s + seeds[i + 1]
-            )
-          );
-
-        return Math.min(
-          ...candidateSeeds.map((val) =>
-            mapMatrix.reduce((curr, mappings) => {
-              const m = mappings.find(
-                (m) => curr >= m.sStart && curr <= m.sEnd
-              );
-              return m ? m.dStart + (curr - m.sStart) : curr;
-            }, val)
-          )
-        );
-      })
-      .then(console.log);
   };
 
   return (
